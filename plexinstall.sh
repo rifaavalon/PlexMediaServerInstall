cd2faddc27709d50928c77563302c49dac17a3bb
Chris Hickey <chris.hickey@xero.com>
2016-12-13 14:40:07 -0700

    added pushover support

--- plexinstall.sh
+++ plexinstall.sh
@@ -4,7 +4,7 @@ echo Get Ready
 sudo apt-get -y update && sudo apt-get -y upgrade
 
 
-sudo apt-get -y install git curl build-essential python-pip
+sudo apt-get -y install git curl build-essential python-pip mailutils
 
 echo Installing Plex Media Server
 curl -O -L https://downloads.plex.tv/plex-media-server/1.3.2.3112-1751929/plexmediaserver_1.3.2.3112-1751929_amd64.deb
@@ -22,3 +22,6 @@ sudo apt-get install transmission-cli transmission-common transmission-daemon
 
 echo Stopping Transmission
 sudo service transmission-daemon stop
+
+curl -s \ --form-string "token=amckugaoetpm5br2siyxb2pjwzgih1" \ --form-string "user=u98uge5qfnuww2v68nqecipp9oy7qh" \ --form-string "message=The Server Is Good to go!" \ https://api.pushover.net/1/messa
+ges.json
